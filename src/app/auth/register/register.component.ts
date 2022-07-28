cf04e9c9efd4de6da1b9581c18daadae9d2ee27d
Alejandro Mira <alejandro.mira7@gmail.com>
2022-07-28 09:38:29 -0500

    avance

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  formSubmited = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    name: [ '', Validators.required ],
+    email: [ '', Validators.required ],
+    password: [ '', Validators.required ],
+    password2: [ '', Validators.required ],
+    terms: [ false, Validators.required ]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  createUser() {
+    console.log(this.registerForm.value);
+    this.formSubmited = true;
+  }
+
+  invalidField(field: string): boolean {
+    if (this.registerForm.get(field)?.invalid && this.formSubmited) {
+      return true;
+    }
+    return false;
   }
 
 }
